1191e77027aac7bca561d5e48bab52c74ee51302
Fernando <bresso.1202@gmail.com>
2015-01-17 21:12:34 -0600

    Programa Fizz Buzz completo

---
+++ FizzBuzz.scala
@@ -0,0 +1,17 @@
+object FizzBuzz {
+  def main(args: Array[String]){
+
+    for(i<-0 to 100){
+      if(i % 3==0 && i % 5==0)
+      println("Fizz Buzz")
+      else
+      if(i % 3==0)
+        println("Fizz")
+      else
+      if(i % 5==0)
+        println("Buzz")
+      else
+        println(" "+i)
+    }
+    }
+}
