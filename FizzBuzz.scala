af299276fecd84f49556f8955b0ff881c3ba027f
JordyCR <jcuan4@gmail.com>
2015-02-09 14:50:59 -0600

    revert

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 /**
  * Created by JordyCuan on 14/ene/15.
  */
@@ -18,37 +17,3 @@ class FizzBuzz extends App{
 		}
 	}
 }
-=======
-object FizzBuzz {
- 
- 
- 
-    
-    //   if(i%5==0&&i%3==0){
-    //  System.out.println("Fizz Buzz");
-    //}
-
-    def  main( args:Array[String]){
-      
-  var i=1
-    
-     for (i<- 1 to 100) {
-    if(i%5==0 && i%3==0){
-     println("Fizz_Buzz")
-    }
-     else{ if (i %3==0)
-      println("Fizz")
-   
-    else{ if(i%5==0)
-      println("Buzz")
-      else {if(i%5!=0 && i%3!=0)
-        println(i)}
-    }
-    }
-    
-     }
-  }
-    
-  
-}
->>>>>>> FETCH_HEAD
