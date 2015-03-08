2253ce399dd9d4fc1950be6505634ee712f63a63
ianbece <adsijer@gmail.com>
2015-03-07 19:12:45 -0600

    Ordenacion Aleatoria en posicion

---
+++ ordenacion_aleatoria/OrdenacionAleatoriaEnPosicion.scala
@@ -0,0 +1,30 @@
+import scala.util.Random
+
+object OrdenacionAleatoria {
+
+  def main(args: Array[String]){
+    
+    var A = Array(9,4,7,3,6,1,0,2,8,5)
+
+    
+    println("Arreglo original")
+    for(i<-0 to A.length-1)
+      print(A(i)+" ")
+    
+    ord_pos(A)  
+    println("\n\nArreglo ordenado")
+    for(i<-0 to A.length-1)
+      print(A(i)+" ")
+  }
+  
+  def ord_pos(A: Array[Int]){
+    var n = A.length;
+    var r = new Random()
+    for(i<-0 to n-1){
+      var ran = r.nextInt(n-1)
+      var aux = A(i)
+      A(i) = A(ran)
+      A(ran) = aux
+    }
+  }
+}
