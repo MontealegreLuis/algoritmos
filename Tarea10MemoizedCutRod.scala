d5bca9b7c071f19637647726f0a6a0b2d31b9d8e
Ramses Hernandez <k_os-mo@hotmail.com>
2015-04-12 22:44:56 -0500

    Tareas de la 7 a la 11

---
+++ Tarea10MemoizedCutRod.scala
@@ -0,0 +1,52 @@
+/*
+  Tarea10 Hacer el MEMOIZED-CUT-ROD en Scala
+   -Mostrar el arreglo de precios.
+   -En cuantas partes se va a cortar.
+   -Arriba-abajo y abajo-arriba
+*/
+object Memorized_Cut_Rod
+{
+  var p = Array(1,5,8,9,10,17,17,20,24,30)
+
+  def main(args: Array[String])
+  {
+    var tubo = 9
+    println("Este es el arreglo de precios: ")
+    for(i<-0 to p.length-1)
+      println("Tamaño: "+(i+1)+" Precio: "+p(i))
+    println("Maxima ganancia de cortar un tubo de: "+tubo);
+    println("Ganancia: "+Memorized_Cut_Rod(tubo))
+  }
+
+  def Memorized_Cut_Rod(n: Int): Int = 
+  {
+    var r = Array.ofDim[Int](n+1)
+    for(i<-0 to n)
+      r(i) = -999999
+    return Memorized_Cut_Rod_Aux(n,r)
+  }
+
+  def Memorized_Cut_Rod_Aux(n: Int,r: Array[Int]): Int = 
+  {
+    if(r(n)>=0)
+      return r(n)
+    if(n==0)
+      return 0
+    else
+    {
+      var q = -999999
+      for(i<-0 to n-1)
+	q = max(q,p(i) + Memorized_Cut_Rod_Aux(n-i-1,r))
+      r(n) = q
+      return q
+    }
+  }
+
+  def max(a: Int, b: Int): Int = 
+  {
+    if( a <= b)
+      return b
+    else
+      return a
+  }
+}//Llave del object
---
+++ Tarea9_CUT-ROD.scala
@@ -0,0 +1,32 @@
+/*
+  Tarea9 CUT-ROD
+*/
+object Cut_Rod
+{
+  //Para no pasarla como argumento se declara aqui
+  var p = Array(1,5,8,9,10,17,17,20,24,30)
+  def main(args: Array[String])
+  {
+    var tubo = 10
+    println("Tamaño del tubo para cortar: "+tubo);
+    println("Ganancia máxima: "+CutRod(tubo))
+  }
+
+  def CutRod(n: Int): Int = 
+  {
+    if(n==0)
+      return 0
+    var q = -9999
+    for(i<-0 to n-1)
+      q = max(q, p(i) + CutRod(n-1-i))
+    return q
+  }
+
+  def max(a: Int, b: Int): Int = 
+  {
+    if( a <= b)
+      return b
+    else
+      return a
+  }
+}
