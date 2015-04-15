4767f4f2ff2fc439de3e498400d225b4886e373b
carlosrn <carlos_lars@hotmail.com>
2015-04-14 22:08:43 -0500

    Tarea Fizz Buzz

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -6,7 +6,7 @@ object FizzBuzz {
            } else if (i % 3 == 0) {
                println ("Fizz: ")
            } else if (i % 5 == 0) {
-               println ("Buzz: ")
+               println ("Buzz:: ")
            } else {
                println(i)
            }
