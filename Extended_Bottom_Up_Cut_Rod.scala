21d9eccf50705b55c455ca9a229181c2a2867308
root <root@debian-hp.buap.com>
2015-04-15 20:07:03 -0500

    Algoritmo en scala Extended_Bottom_Up_Cut_Rod corregido

--- Extended_Bottom_Up_Cut_Rod.scala
+++ Extended_Bottom_Up_Cut_Rod.scala
@@ -3,7 +3,7 @@ object Extended_Bottom_Up_Cut_Rod{
    def main(args:Array[String]){
      	var P = Array(1,5,8,9,10,17,17,20,24,30)
 	var n= 4
-
+        println("Los cortes son = ")
           Extended_BottomUpCutRod(P,n)
 	//var resp= Extended_BottomUpCutRod(P,n)
 	//println("resultado= "+resp)
@@ -17,18 +17,18 @@ object Extended_Bottom_Up_Cut_Rod{
 	var q=0
         r(0)=0
          
-	for(j <- 0 to m){
+	for(j <- 1 to m){
 	    q=(-999)
-	    for(i<-0 to j){
+	    for(i<-1 to j){
               if(q<(P(i-1)+r(j-i))){  
                  q=(P(i-1)+r(j-i))
                  s(j)=i
-                 println("s(j)= "+s(j))
+                 //println("s(j)= "+s(j))
 
               }    
             }
 	    r(j)=q
-	    println("r(j)= "+r(j))
+//	    println("r(j)= "+r(j))
 	}
 
 	while(m>0){
