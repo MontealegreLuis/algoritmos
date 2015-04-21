d4d20fe7a792e5a201462155bc7776a4ab6eeeeb
Azael Ortega <azael_valdo@hotmail.com>
2015-04-21 03:11:21 -0500

    Entrega de tarea 1

--- FizzBuzz.scala
+++ FizzBuzz.scala
@@ -1,15 +1,19 @@
-object FizzBuzz {
-    def main(args: Array[String]) {
-       for (i <- 0 to 100) {
-           if (i % 3 == 0 && i % 5 == 0) {
-               println ("FizzBuzz")
-           } else if (i % 3 == 0) {
-               println ("Fizz")
-           } else if (i % 5 == 0) {
-               println ("Buzz")
-           } else {
-               println(i)
-           }
-       }
+object FizzBuzz{
+def main(args:Array[String]){
+	for(i<-0 to 100){
+		if(i%5==0 && i%3==0){
+			println("FizzBuzz")
+		}
+		
+        else if(i%3==0){
+			println("Fizz")
+		}
+		else if(i%5==0){
+                        println("Buzz")
+        }
+		else{
+			println(i)
+	    }
     }
+}
 }
